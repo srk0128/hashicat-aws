34098e2649de4b7ff56c870838b0e9012a248364
snehak <sneha0128reddy@yahoo.com>
2022-04-27 21:17:22 +0000

    added s3again

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/sk-cdwlab/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket_perfix = "snehak"
+  # bucket_perfix = "snehak"
+  bucket_prefix = var.prefix
 }
